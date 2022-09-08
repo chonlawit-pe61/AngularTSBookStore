c9f9c827c989594443ca3157de8ddd848d117511
Chonlawit penwijit <DISTSY@gmail.com>
2022-09-08 21:34:36 +0700

    Login/Logout

--- src/app/interfaces/interface.ts
+++ src/app/interfaces/interface.ts
@@ -6,3 +6,12 @@ export interface DataUser {
   id: string;
   data: string;
 }
+export interface DataUserRegister {
+  id: string;
+  username: string;
+  passwordSalt: string;
+  passwordHash: string;
+  name: string;
+  lname: string;
+  role: string;
+}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataUserRegister } from 'src/app/interfaces/interface';
 
 @Component({
   selector: 'app-register',
@@ -14,8 +17,30 @@ export class RegisterComponent implements OnInit {
     name: new FormControl(null, Validators.required),
     lname: new FormControl(null, Validators.required),
   });
-  constructor() {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {}
-  onSubmit() {}
+  onSubmit() {
+    const { username, password, role, name, lname } =
+      this.formUserRegister.value;
+    const obj = {
+      username: username,
+      Role: role,
+      password: password,
+      name: name,
+      lname: lname,
+    };
+    console.log(typeof obj.password);
+    this.http
+      .post<DataUserRegister>('https://localhost:7045/api/User/CreateUser', obj)
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.router.navigate(['/login']);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
 }
